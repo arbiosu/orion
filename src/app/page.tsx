7560f0e1c36c078d73ce7d205cd26d376ff29886
Arberim Ame <amea@oregonstate.edu>
2025-11-05 23:15:53 -0500

    slight changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 import Hero from '@/components/hero';
+import Services from '@/components/services';
+import About from '@/components/about';
+import Reviews from '@/components/reviews-carousel';
+import BentoGridGallery from '@/components/bento-image-grid';
+
+import { images } from '@/lib/constants';
+
 export default function Home() {
   return (
-    <main>
+    <main className='flex flex-col gap-12'>
       <Hero />
+      <Services />
+      <About />
+      <Reviews />
+      <BentoGridGallery images={images} />
     </main>
   );
 }
---
+++ src/lib/constants.ts
@@ -0,0 +1,26 @@
+export const images = [
+  {
+    src: '/shed.jpeg',
+    alt: 'Shed by Orion Remodeling & Handyman',
+    title: 'Shed',
+    className: 'aspect-square',
+  },
+  {
+    src: '/kitchen.jpeg',
+    alt: 'Kitchen by Orion Remodeling & Handyman',
+    title: 'Kitchen Renovation',
+    className: 'aspect-[2/1] col-span-2',
+  },
+  {
+    src: '/renovation.jpeg',
+    alt: 'Renovation by Orion Remodeling & Handyman',
+    title: 'Living Room Renovation',
+    className: 'aspect-[2/1] col-span-2',
+  },
+  {
+    src: '/bathroom.jpeg',
+    alt: 'Bathroom by Orion Remodeling & Handyman',
+    title: 'Bathroom Renovation',
+    className: 'aspect-square',
+  },
+];
